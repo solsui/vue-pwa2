5654ba2b1600565382ea9314c29d0cd66ea31214
Takashi Inada <takashi.inada@hde.co.jp>
2018-09-27 16:42:35 +0900

    Updates

---
+++ static/myPush.js
@@ -0,0 +1,17 @@
+self.addEventListener("push", function(event) {
+    event.waitUntil(
+      self.registration.pushManager.getSubscription()
+        .then(function(subscription) {
+          if (subscription) {
+            return subscription.endpoint
+          }
+          throw new Error('User not subscribed')
+      })
+      .then(function(res) {
+
+        return self.registration.showNotification('title', {
+          body: 'contents'
+        })
+      })
+    )
+})
